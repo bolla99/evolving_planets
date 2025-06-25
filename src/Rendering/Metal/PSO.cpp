d24f27a128a9c6f054e54b1141b7919058df0ff9
bolla99 <46454991+bolla99@users.noreply.github.com>
2025-06-25 16:18:24 +0200

    -

---
+++ src/Rendering/Metal/PSO.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Giovanni Bollati on 12/06/25.
+//
+
+#include "../../../include/Rendering/Metal/PSO.hpp"
+#include <iostream>
+#include <string>
+#include "../../../include/Rendering/VertexDescriptorUtils.hpp"
+
+namespace Rendering::Metal
+{
+    PSO::PSO(
+        const PSOConfig& config,
+        MTL::Device* device,
+        MTL::Library* library,
+        MTL::PixelFormat pixelFormat
+        ) : IPSO(config)
+    {
+        _vertexF = NS::TransferPtr(library->newFunction(
+            NS::String::string(config.vertexShader.c_str(), NS::ASCIIStringEncoding)));
+        if (!_vertexF)
+        {
+            throw std::runtime_error("Failed to create vertex function with name: " + config.vertexShader);
+        }
+        _fragmentF = NS::TransferPtr(library->newFunction(
+            NS::String::string(config.fragmentShader.c_str(), NS::ASCIIStringEncoding)));
+        if (!_fragmentF)
+        {
+            throw std::runtime_error("Failed to create fragment function with name: " + config.fragmentShader);
+        }
+
+        const auto psoDescriptor = NS::TransferPtr(MTL::RenderPipelineDescriptor::alloc()->init());
+        if (!psoDescriptor)
+        {
+            throw std::runtime_error("Failed to create pipeline descriptor");
+        }
+
+        psoDescriptor->setVertexDescriptor(createVertexDescriptor(config.vertexDescriptor).get());
+
+        psoDescriptor->setLabel(NS::String::string(config.name.c_str(), NS::ASCIIStringEncoding));
+        psoDescriptor->setVertexFunction(_vertexF.get());
+        psoDescriptor->setFragmentFunction(_fragmentF.get());
+        psoDescriptor->colorAttachments()->object(0)->setPixelFormat(pixelFormat);
+        psoDescriptor->setDepthAttachmentPixelFormat(MTL::PixelFormat::PixelFormatDepth32Float);
+
+        NS::Error* error;
+        _metalPSO = NS::TransferPtr(device->newRenderPipelineState(psoDescriptor.get(), &error));
+
+        if (!_metalPSO)
+        {
+            throw std::runtime_error(error->localizedDescription()->utf8String());
+        }
+    }
+}
