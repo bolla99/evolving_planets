14853fdfb7ecad296278b50d24a2bb6b47a3234a
bolla99 <46454991+bolla99@users.noreply.github.com>
2026-02-06 17:44:14 +0100

    new app

---
+++ include/RTGPApp.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Giovanni Bollati on 29/01/26.
+//
+
+#ifndef EVOLVING_PLANETS_RTGPAPP_HPP
+#define EVOLVING_PLANETS_RTGPAPP_HPP
+
+#include "App.hpp"
+
+class RTGPApp : public App
+{
+public:
+    RTGPApp(int width, int height) : App(width, height) {};
+    void init() override;
+    void run() override;
+};
+
+
+#endif //EVOLVING_PLANETS_RTGPAPP_HPP
