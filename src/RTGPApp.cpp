2f0b75fd9605a0162d4e170195ecf97bc4a755e9
bolla99 <46454991+bolla99@users.noreply.github.com>
2026-05-20 22:04:03 +0200

    Last RTGP App
    added planet and skybox

--- src/RTGPApp.cpp
+++ src/RTGPApp.cpp
@@ -7,34 +7,40 @@
 
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
-#include "../include/Engine/Pool.hpp"
-#include "../include/Engine/Storage.hpp"
+#include "Engine/ECS/Pool.hpp"
+#include "Engine/ECS/Storage.hpp"
 #include "TrackballCamera.hpp"
-#include "../include/Engine/World.hpp"
+#include "Engine/ECS/World.hpp"
 #include "Apple/Util.hpp"
 #include "glm/gtc/type_ptr.hpp"
-#include "../include/Engine/Components.hpp"
-#include <Engine/Systems.hpp>
+#include "../include/Engine/ECS/Components.hpp"
+#include <Engine/ECS/Systems.hpp>
 
-#include "Engine/SystemsManager.hpp"
+#include "timer.hpp"
+#include "../include/Engine/ECS/SystemsManager.hpp"
 #include "Engine/UI/ComponentsInspector.hpp"
 
+#include <btBulletCollisionCommon.h>
+#include <btBulletDynamicsCommon.h>
+
+#include "Engine/ECS/Systems/PhysicsSystem.hpp"
+#include "Engine/ECS/Systems/PlanetSystem.hpp"
+
 void RTGPApp::init()
-{
-    // RENDER AXES
-    std::vector<glm::vec3> x = {{0.0f, 0.0f, 0.0f}, {1000.0f, 0.0f, 0.0f}};
-    std::vector<glm::vec3> y = {{0.0f, 0.0f, 0.0f}, {0.0f, 1000.0f, 0.0f}};
-    std::vector<glm::vec3> z = {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1000.0f}};
-    auto xMesh = Mesh::fromPolygon(x, {1.0f, 0.0f, 0.0f, 1.0f}, false);
-    auto yMesh = Mesh::fromPolygon(y, {0.0f, 1.0f, 0.0f, 1.0f}, false);
-    auto zMesh = Mesh::fromPolygon(z, {0.0f, 0.0f, 1.0f, 1.0f}, false);
-    _renderer->addRenderable(*xMesh, Rendering::psoConfigs.at("curve"), {}, Rendering::RenderLayer::OPAQUE);
-    _renderer->addRenderable(*yMesh, Rendering::psoConfigs.at("curve"), {}, Rendering::RenderLayer::OPAQUE);
-    _renderer->addRenderable(*zMesh, Rendering::psoConfigs.at("curve"), {}, Rendering::RenderLayer::OPAQUE);
-}
+{}
 
 void RTGPApp::run()
 {
+    // INIT BULLET
+    auto* collisionConfiguration = new btDefaultCollisionConfiguration();
+    auto dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    auto overlappingPairCache = new btDbvtBroadphase();
+    auto solver = new btSequentialImpulseConstraintSolver();
+
+    auto* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+    dynamicsWorld->setGravity(btVector3(0, -9.81f, 0));
+
+
     // CREATE WORLD AND CONTEXT
     auto world = World();
     auto ctx = Context{_renderer.get(), _window, &_assetManager};
@@ -42,41 +48,87 @@ void RTGPApp::run()
 
     // SYSTEMS MANAGER
     auto systemManager = SystemsManager();
+    // LOADING AND REGISTRATION
+    systemManager.addSystem<PlanetGeneratorSystem>();
     systemManager.addSystem<MeshLoadingSystem>();
+    systemManager.addSystem<TextureLoadingSystem>();
     systemManager.addSystem<RenderRegistrationSystem>();
-    systemManager.addSystem<TransformSystem>();
+    systemManager.addSystem<BoundingSphereRequestSystem>();
+    // GAMEPLAY SYSTEMS
     systemManager.addSystem<MouseRaySystem>();
-    systemManager.addSystem<UpdateLightsWithTransformSystem>();
-    systemManager.addSystem<SetLightsSystem>();
     systemManager.addSystem<MouseIntersectionSystem>();
-    systemManager.addSystem<UpdateMaterialTintSystem>();
-    systemManager.addSystem<RendererUpdateSystem>();
+    // UPDATE MATERIALS
+    systemManager.addSystem<PlanetMaterialSystem>();
+    systemManager.addSystem<UpdateWardMaterialSystem>();
     systemManager.addSystem<RectMaterialComponentSystem>();
     systemManager.addSystem<ViewportSizeMaterialSystem>();
+    // UPDATE PHYSICS
+    systemManager.addSystem<PhysicsSystem>();
+    // APPLY TRANSFORM TO GRAPHICS
+    systemManager.addSystem<TransformSystem>();
+    systemManager.addSystem<UpdateCameraTransformSystem>();
+    // UPDATE LIGHTS
+    systemManager.addSystem<LightAndShadowSystem>();
+    // GRAPHICS UPDATE
+    systemManager.addSystem<RendererUpdateSystem>();
 
     // ENTITIES
+    // RENDER AXES
+    std::vector<glm::vec3> x = {{0.0f, 0.0f, 0.0f}, {10000.0f, 0.0f, 0.0f}};
+    std::vector<glm::vec3> y = {{0.0f, 0.0f, 0.0f}, {0.0f, 10000.0f, 0.0f}};
+    std::vector<glm::vec3> z = {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 10000.0f}};
+    auto xMesh = Mesh::fromPolygon(x, {1.0f, 0.0f, 0.0f, 1.0f}, false);
+    auto yMesh = Mesh::fromPolygon(y, {0.0f, 1.0f, 0.0f, 1.0f}, false);
+    auto zMesh = Mesh::fromPolygon(z, {0.0f, 0.0f, 1.0f, 1.0f}, false);
+
     auto viewport = world.createEntity("viewport");
     world.addComponent<ViewportComponent>(viewport, ViewportComponent());
 
+    auto planet = world.createEntity("planet");
+    world.addComponent<PlanetConfigComponent>(planet, PlanetConfigComponent());
+    world.addComponent<Transform>(planet, Transform());
+    world.addComponent<PlanetInfoComponent>(planet, PlanetInfoComponent());
+
     // background
-    auto background = world.createEntity("background");
-    world.addComponent<RectMaterialComponent>(background, RectMaterialComponent(RectMaterial({0.0f, 0.0f, 800.0f, 600.0f})));
-    auto quad = Mesh::quad({0.0f, 0.0f, 1.0f, 1.0f}, 0.9f, {1.0f, 0.0f, 0.0f, 1.0f}, 1.0f, 1.0f);
+    auto background = world.createEntity("sky");
+    world.addComponent<RectMaterialComponent>(background, RectMaterialComponent(RectMaterial({0.0f, 0.0f, 4000.0f, 4000.0f})));
+    auto quad = Mesh::quad({0.0f, 0.0f, 1.0f, 1.0f}, 0.9999f, {0.0f, 1.0f, 0.0f, 1.0f}, 1.0f, 1.0f);
     world.addComponent<MeshComponent>(background, MeshComponent(quad, "quad"));
-    world.addComponent<RenderConfigComponent>(background, {"UI", Rendering::RenderLayer::BACKGROUND});
+    world.addComponent<RenderConfigComponent>(background, {"Skybox", false, Rendering::RenderLayer::BACKGROUND});
+    world.addComponent<RenderRequestComponent>(background, RenderRequestComponent());
     world.addComponent<ViewportSizeMaterialComponent>(background, ViewportSizeMaterialComponent());
 
+    /*
     // LIGHT ENTITIES
-    auto pointLight = world.createEntity("point light");
-    world.addComponent(pointLight, PointLightComponent({{0.0f, 0.0f, 2.0f, 1.0f}, {0.5f, 0.1f, 0.1f, 1.0f}}));
-    world.addComponent(pointLight, Transform({.position = {0.0f, 0.0f, 2.0f}, .scale = {0.1f, 0.1f, 0.1f}, }));
-    world.addComponent<MeshRequestComponent>(pointLight, {Apple::resourcePath("ico.fbx")});
-    world.addComponent<RenderConfigComponent>(pointLight, {"VCPHONG_WITH_TINT", Rendering::RenderLayer::OPAQUE});
+    auto pointLight1 = world.createEntity("point light 1");
+    world.addComponent(pointLight1, PointLightComponent({{0.0f, 0.0f, 2.0f, 1.0f}, {0.5f, 0.1f, 0.1f, 1.0f}}));
+    world.addComponent(pointLight1, Transform({.position = {-2.0f, 0.0f, 2.0f}, .scale = {0.1f, 0.1f, 0.1f}, }));
+    world.addComponent<MeshRequestComponent>(pointLight1, {Apple::resourcePath("ico.fbx")});
+    world.addComponent<RenderRequestComponent>(pointLight1, RenderRequestComponent());
+    world.addComponent<RenderConfigComponent>(pointLight1, {"VCPHONG", true, Rendering::RenderLayer::OPAQUE});
 
-    auto directionalLight = world.createEntity("sun");
-    world.addComponent(directionalLight, DirectionalLightComponent());
-    world.getComponent<DirectionalLightComponent>(directionalLight).light.color = {0.2f, 0.2f, 0.2f, 1.0f};
+    auto pointLight2 = world.createEntity("point light 2");
+    world.addComponent(pointLight2, PointLightComponent({{0.0f, 0.0f, 2.0f, 1.0f}, {0.5f, 0.1f, 0.1f, 1.0f}}));
+    world.addComponent(pointLight2, Transform({.position = {2.0f, 0.0f, 2.0f}, .scale = {0.1f, 0.1f, 0.1f}, }));
+    world.addComponent<MeshRequestComponent>(pointLight2, {Apple::resourcePath("ico.fbx")});
+    world.addComponent<RenderRequestComponent>(pointLight2, RenderRequestComponent());
+    world.addComponent<RenderConfigComponent>(pointLight2, {"VCPHONG", true, Rendering::RenderLayer::OPAQUE});
+    */
 
+    auto ambientLight = world.createEntity("ambient light");
+    world.addComponent(ambientLight, AmbientLightComponent({0.1f, 0.1f, 0.1f}));
+
+    auto sun1 = world.createEntity("sun1");
+    world.addComponent(sun1, DirectionalLightComponent());
+    world.getComponent<DirectionalLightComponent>(sun1).light.color = {8.0f, 8.0f, 8.0f, 1.0f};
+    world.getComponent<DirectionalLightComponent>(sun1).light.direction = {0.9f, -1.0f, 0.8, 0.0f};
+
+    /*
+    auto sun2 = world.createEntity("sun2");
+    world.addComponent(sun2, DirectionalLightComponent());
+    world.getComponent<DirectionalLightComponent>(sun2).light.color = {1.0f, 1.0f, 1.0f, 1.0f};
+    world.getComponent<DirectionalLightComponent>(sun2).light.direction = {-0.9f, -1.0f, 0.8, 0.0f};
+    */
 
     // MOUSE RAY ENTITY
     auto mouseRay = world.createEntity("mouse ray");
@@ -84,22 +136,49 @@ void RTGPApp::run()
 
     // CAMERA ENTITY
     auto cameraID = world.createEntity("camera");
-    world.addComponent<CameraComponent>(cameraID, {std::make_shared<TrackballCamera>()});
-    static_pointer_cast<TrackballCamera>(world.getComponent<CameraComponent>(world.query<CameraComponent>()[0]).camera)->zoom(5.0f);
-    auto& camera = *world.getComponent<CameraComponent>(cameraID).camera;
+    world.addComponent<Transform>(cameraID, Transform());
+    world.addComponent(cameraID, PointLightComponent({{0.0f, 0.0f, 2.0f, 1.0f}, {10.0f, 10.0, 10.0f, 1.0f}}));
+    auto& cameraComponent = world.addComponent<CameraComponent>(cameraID, {std::make_shared<FPSCamera>()});
+    auto camera = cameraComponent.camera;
+    camera->position = {0.0f, 0.0f, 550.0f};
 
-    auto cameraSpeed = 100.0f;
+    auto cameraSpeed = 5.0f;
+    bool isCursorActive = true;
+    SDL_SetRelativeMouseMode(SDL_FALSE);
 
     // selected entity
     uint64_t selectedEntity = 0;
 
-    // MONKEY ENTITY
-    auto monkey = world.createEntity("monkey");
-    world.addComponent<MeshRequestComponent>(monkey, {Apple::resourcePath("monkey.obj")});
-    world.addComponent<RenderConfigComponent>(monkey, {"VCPHONG_WITH_TINT", Rendering::RenderLayer::OPAQUE});
-    world.addComponent<Transform>(monkey, Transform());
-    world.addComponent<MouseRayIntersectionComponent>(monkey, MouseRayIntersectionComponent());
-    world.addComponent<TintMaterialComponent>(monkey, TintMaterialComponent());
+    /*
+    auto ball1 = world.createEntity("ball");
+    world.addComponent<MeshRequestComponent>(ball1, {Apple::resourcePath("cagnaccio1.obj")});
+    world.addComponent<RenderRequestComponent>(ball1, RenderRequestComponent());
+    world.addComponent<RenderConfigComponent>(ball1, {"VCWARD_FULLSHADOW", true, Rendering::RenderLayer::OPAQUE});
+    auto& transform = world.addComponent<Transform>(ball1, Transform());
+    transform.position = {0.0f, 0.0f, 0.0f};
+    world.addComponent<WardMaterialComponent>(ball1, WardMaterialComponent());
+    auto& rb = world.addComponent<RigidBodyComponent>(ball1, RigidBodyComponent(ColliderType::SPHERE));
+    rb.mass = 10.0f;
+    rb.bounciness = 0.0f;
+    rb.friction = 0.3f;
+    rb.radius = 1.0f;
+    */
+
+    /*
+    auto plane = world.createEntity("plane");
+    world.addComponent<MeshRequestComponent>(plane, {Apple::resourcePath("plane.obj")});
+    world.addComponent<RenderRequestComponent>(plane, RenderRequestComponent());
+    world.addComponent<RenderConfigComponent>(plane, {"VCWARD_FULLSHADOW", true, Rendering::RenderLayer::OPAQUE});
+    world.addComponent<Transform>(plane, Transform());
+    world.addComponent<WardMaterialComponent>(plane, WardMaterialComponent());
+    world.addComponent<RigidBodyComponent>(plane, RigidBodyComponent(ColliderType::BOX));
+    auto& rb2 = world.getComponent<RigidBodyComponent>(plane);
+    rb2.mass = 0.0f;
+    rb2.boxHalfExtents = {10.0f, 0.3f, 10.0f};
+    rb2.isKinematic = true;
+    rb2.friction = 0.3f;
+    */
+
 
     SDL_Event event;
     auto running = true;
@@ -112,6 +191,11 @@ void RTGPApp::run()
 
     while (running)
     {
+        // DRAW AXES
+        ctx.renderer->iDraw(*xMesh, {}, "curve", {});
+        ctx.renderer->iDraw(*yMesh, {}, "curve", {});
+        ctx.renderer->iDraw(*zMesh, {}, "curve", {});
+
         bool leftMouseButtonDown = false;
 
         auto now = SDL_GetPerformanceCounter();
@@ -124,9 +208,27 @@ void RTGPApp::run()
             {
                 running = false;
             }
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (ImGui::GetIO().WantCaptureMouse) continue;
+            if (isCursorActive)
+            {
+                ImGui_ImplSDL2_ProcessEvent(&event);
+                if (ImGui::GetIO().WantCaptureMouse) continue;
+            }
 
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.scancode == SDL_SCANCODE_SPACE)
+                {
+                    isCursorActive = !isCursorActive;
+                    SDL_SetRelativeMouseMode(isCursorActive ? SDL_FALSE : SDL_TRUE);
+                }
+            }
+
+            if (!isCursorActive && event.type == SDL_MOUSEMOTION)
+            {
+                float sensitivity = 0.002f;
+                camera->pan(-static_cast<float>(event.motion.xrel) * sensitivity);
+                camera->tilt(-static_cast<float>(event.motion.yrel) * sensitivity);
+            }
 
             if (event.type == SDL_MOUSEMOTION && (event.motion.state & SDL_BUTTON(SDL_BUTTON_LEFT)))
             {
@@ -146,32 +248,56 @@ void RTGPApp::run()
             int* length = nullptr;
             auto keyboardState = SDL_GetKeyboardState(length);
 
-            auto velocity = glm::vec2(0.0f);
-            auto speed = 5.0f;
+            auto actualCamSpeed = cameraSpeed;
+            if (keyboardState[SDL_SCANCODE_X]) actualCamSpeed *= 10.0f;
+
+            auto velocity = glm::vec2({0.0f, 0.0f});
             if (keyboardState[SDL_SCANCODE_LEFT]) velocity.x -= 1.0f;
             if (keyboardState[SDL_SCANCODE_RIGHT]) velocity.x += 1.0f;
             if (keyboardState[SDL_SCANCODE_UP]) velocity.y += 1.0f;
             if (keyboardState[SDL_SCANCODE_DOWN]) velocity.y -= 1.0f;
-            velocity = glm::normalize(velocity);
-            camera.advance(velocity.y * speed * deltaTime);
-            camera.strafe(velocity.x * speed * deltaTime);
+            if (glm::length(velocity) > 0.0f)
+            {
+                velocity = glm::normalize(velocity);
+            }
+            camera->advance(velocity.y * actualCamSpeed * deltaTime);
+            camera->strafe(velocity.x * actualCamSpeed * deltaTime);
+
+            static auto angle_z = 0.0f;
+            static auto angle_x = 0.0f;
+            /*
+            if (keyboardState[SDL_SCANCODE_LEFT]) angle_z += deltaTime * 20.0f;
+            if (keyboardState[SDL_SCANCODE_RIGHT]) angle_z -= deltaTime * 20.0f;
+            if (keyboardState[SDL_SCANCODE_UP]) angle_x -= deltaTime * 20.0f;
+            if (keyboardState[SDL_SCANCODE_DOWN]) angle_x += deltaTime * 20.0f;
+            */
+            //std::cout << "angle_z: " << angle_z << ", angle_x: " << angle_x << std::endl;
+
+            /*
+            auto& planeTransform = world.getComponent<Transform>(plane);
+            planeTransform.rotation = glm::rotate({1.0f, 0.0f, 0.0f, 0.0f}, glm::radians(angle_z), glm::vec3(0.0f, 0.0f, 1.0f));
+            planeTransform.rotation = glm::rotate(planeTransform.rotation, glm::radians(angle_x), glm::vec3(1.0f, 0.0f, 0.0f));
+            */
+
 
 
             //SDL_GetRelativeMouseState(&mouseDX, &mouseDY);
             // apply accumulated mouse movement
-            camera.pan(-mouseDX * 0.005f * cameraSpeed);
-            camera.tilt(-mouseDY * 0.005f * cameraSpeed);
             mouseDX = 0.0;
             mouseDY = 0.0;
         }
 
-        world.getComponent<DirectionalLightComponent>(directionalLight).light.direction = glm::inverse(camera.getViewMatrix()) * glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+        //world.getComponent<DirectionalLightComponent>(directionalLight).light.direction = glm::inverse(camera.getViewMatrix()) * glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
 
           ///////////////////////////////////////////////////////////////////////////////
          /////////////////////////////////// UI // /////////////////////////////////////
         ///////////////////////////////////////////////////////////////////////////////
-        _renderer->setDebugUICallback([this, &world, &selectedEntity, &ctx, inspector]
+        _renderer->setDebugUICallback([this, &world, &selectedEntity, &ctx, inspector, deltaTime]
         {
+            ImGui::Begin("General Info");
+            ImGui::Text("Delta Time: %.3f ms", deltaTime * 1000.0f);
+            ImGui::End();
+
             ImGui::Begin("Entities");
             ImGui::Text("Selected Entity: %lu", selectedEntity);
             for (uint64_t e : world.getEntities())
