86f939920b5c1fa458c4ae762961ece51eb9feb2
bolla99 <46454991+bolla99@users.noreply.github.com>
2026-06-27 16:01:46 +0200

    temporal scaling + directional light passes + material updaters

--- include/Engine/ECS/Systems.hpp
+++ include/Engine/ECS/Systems.hpp
@@ -46,6 +46,7 @@ struct TransformSystem : public ISystem
             if (world.hasComponent<MaterialComponent>(entity))
             {
                 auto& material = world.getComponent<MaterialComponent>(entity);
+                ctx.renderer->setInstanceMaterial(material.id, getBytes(transform.previousModelMatrix), MaterialType::PREVIOUS_MODEL_MATRIX);
                 ctx.renderer->setInstanceMaterial(material.id, getBytes(transform.modelMatrix()), MaterialType::MODEL_MATRIX);
                 ctx.renderer->setInstanceMaterial(material.id, getBytes(glm::inverseTranspose(transform.modelMatrix())), MaterialType::NORMAL_MATRIX);
             }
@@ -59,6 +60,8 @@ struct TransformSystem : public ISystem
                 auto& light = world.getComponent<DirectionalLightComponent>(entity).light;
                 light.direction = glm::normalize(transform.rotation * light.direction);
             }
+
+            transform.previousModelMatrix = transform.modelMatrix();
         }
     }
     [[nodiscard]] std::string name() const override { return "TransformSystem"; }
@@ -141,18 +144,29 @@ struct LightAndShadowSystem : ISystem
         for (int i = 0; i < directionalLightsSize; i++)
         {
             auto lightComp = world.getComponent<DirectionalLightComponent>(directionalLights[i]);
-            lightComp.light.color *= lightComp.intensity;
-            lights.directionalLights[i] = lightComp.light;
-            lights.numDirectionalLights++;
+            // loop through the passess for a single light
+            for (int p = 0; p < lightComp.nPasses; p++)
+            {
+                lightComp.light.color *= lightComp.intensity;
+                lights.directionalLights[i] = lightComp.light;
+                lights.numDirectionalLights++;
 
+                auto dir = glm::normalize(glm::vec3(lightComp.light.direction));
 
-            auto dir = glm::vec3(lightComp.light.direction);
-            auto up = glm::vec3(0.0f, 1.0f, 0.0f);
-            if (std::abs(glm::dot(glm::normalize(dir), up)) > 0.99f) up = glm::vec3(0.0f, 0.0f, 1.0f);
-            auto lightViewMatrix = glm::lookAt(-dir * lightComp.distance, dir, up);
-            auto d = lightComp.orthoSize;
-            auto orthoProjMatrix = glm::orthoRH_ZO(-1.0f * d, d, -1.0f * d, d, lightComp.nearPlane, lightComp.farPlane);
-            shadowData.lightViewMatrix[i] = orthoProjMatrix * lightViewMatrix;
+                // set first directional light as sun_direction
+                if (i == 0)
+                {
+                    ctx.renderer->setGlobalMaterial(getBytes(glm::vec4(dir, 0.0f)), SUN_DIRECTION);
+                    ctx.renderer->setGlobalMaterial(getBytes(lightComp.light.color * lightComp.intensity), SUN_COLOR);
+                }
+                auto settings = lightComp.settings[p];
+                auto up = glm::vec3(0.0f, 1.0f, 0.0f);
+                if (std::abs(glm::dot(glm::normalize(dir), up)) > 0.99f) up = glm::vec3(0.0f, 0.0f, 1.0f);
+                auto lightViewMatrix = glm::lookAt(-dir * settings.distance + settings.center, settings.center, up);
+                auto d = settings.orthoSize;
+                auto orthoProjMatrix = glm::orthoRH_ZO(-1.0f * d, d, -1.0f * d, d, settings.nearPlane, settings.farPlane);
+                shadowData.lightViewMatrix[i + p] = orthoProjMatrix * lightViewMatrix;
+            }
         }
 
         auto pointShadowData = PointShadowData();
@@ -165,10 +179,10 @@ struct LightAndShadowSystem : ISystem
 
             auto fov = 90.0f;
             auto aspect = 1.0f;
-            auto nearPlane = 0.1f;
-            auto farPlane = 50.0f;
+            auto nearPlane = 0.01f;
+            auto farPlane = 100.0f;
             auto lightProjMatrix = glm::perspectiveRH_ZO(glm::radians(fov), aspect, nearPlane, farPlane);
-            lightProjMatrix[1][1] *= -1;
+            //lightProjMatrix[1][1] *= -1;
             auto pos = glm::vec3(lightComp.light.position);
             auto viewMatrix1 = glm::lookAt(pos, pos + glm::vec3(1.0f, 0.0f, 0.0f), {0.0f, -1.0f, 0.0f});
             auto viewMatrix2 = glm::lookAt(pos, pos + glm::vec3(-1.0f, 0.0f, 0.0f), {0.0f, -1.0f, 0.0f});
@@ -296,10 +310,46 @@ struct MeshLoadingSystem : ISystem
     }
     std::string name() const override { return "MeshLoadingSystem"; }
 };
+
 struct RendererUpdateSystem : ISystem
 {
+    glm::mat4 previousViewProjectionMatrix = glm::mat4(1.0f);
+
     void update(World& world, const Context& ctx, float dt) override
     {
+        auto drawableSize = ctx.renderer->getDrawableSize();
+        // JITTER
+        // Puoi metterli in una variabile globale o statica
+        static int frameIndex = 0;
+        frameIndex = (frameIndex + 1) % 32; // Cicla tra 0 e 15
+        // Esempio di sequenza di Halton precalcolata (in range -0.5 a +0.5 pixel)
+        // Sequenza corretta e scalata tra -0.5 e +0.5
+        static const float haltonX[32] = {
+            0.00000f, -0.50000f,  0.25000f, -0.25000f,  0.37500f, -0.12500f,  0.12500f, -0.37500f,
+            0.43750f, -0.06250f,  0.18750f, -0.31250f,  0.31250f, -0.18750f,  0.06250f, -0.43750f,
+            0.46875f, -0.03125f,  0.21875f, -0.28125f,  0.34375f, -0.15625f,  0.09375f, -0.40625f,
+            0.40625f, -0.09375f,  0.15625f, -0.34375f,  0.28125f, -0.21875f,  0.03125f, -0.46875f
+        };
+
+        static const float haltonY[32] = {
+            -0.16667f,  0.16667f, -0.38889f,  0.05556f,  0.38889f, -0.27778f,  0.05556f,  0.27778f,
+            -0.46296f, -0.12963f,  0.20370f, -0.35185f, -0.01852f,  0.31481f, -0.24074f,  0.09259f,
+             0.42593f, -0.31481f,  0.01852f,  0.35185f, -0.20370f,  0.12963f,  0.46296f, -0.42593f,
+            -0.09259f,  0.24074f, -0.31481f,  0.01852f,  0.35185f, -0.20370f,  0.12963f,  0.46296f
+        };
+        // jitter in pixel space
+        float jitterX_pixel = haltonX[frameIndex];
+        float jitterY_pixel = haltonY[frameIndex];
+        // apply jitter
+        auto TAAScaling = ctx.renderer->getTAAScaling();
+        int internalWidth = floor(drawableSize[0] * TAAScaling);
+        int internalHeight = floor(drawableSize[1] * TAAScaling);
+        glm::vec2 jitterNDC = {
+            jitterX_pixel * 2.0f / static_cast<float>(internalWidth),
+            -jitterY_pixel * 2.0f / static_cast<float>(internalHeight)
+        };
+        ctx.renderer->setJitter(glm::vec2(jitterX_pixel, jitterY_pixel));
+
         Timer t1;
         auto cameras = world.query<CameraComponent>();
         if (cameras.empty()) return;
@@ -307,7 +357,6 @@ struct RendererUpdateSystem : ISystem
         auto viewports = world.query<ViewportComponent>();
         if (viewports.empty()) return;
         auto vp = world.getComponent<ViewportComponent>(viewports[0]);
-        auto drawableSize = ctx.renderer->getDrawableSize();
         auto [aspectRatio, normalizedViewport] = vp.getData({drawableSize[0], drawableSize[1]});
         auto projectionMatrix = glm::perspectiveRH_ZO(glm::radians(camera->fov), aspectRatio, camera->nearPlane, camera->farPlane);
 
@@ -329,7 +378,29 @@ struct RendererUpdateSystem : ISystem
         ctx.renderer->setGlobalMaterial(getBytes(glm::inverse(camera->getViewMatrix())), MaterialType::INVERSE_VIEW_MATRIX);
         ctx.renderer->setGlobalMaterial(getBytes(glm::inverse(projectionMatrix)), MaterialType::INVERSE_PROJECTION_MATRIX);
         ctx.renderer->setGlobalMaterial(getBytes(projectionMatrix * camera->getViewMatrix()), VIEW_PROJECTION_MATRIX);
+        ctx.renderer->setGlobalMaterial(getBytes(glm::inverse(projectionMatrix * camera->getViewMatrix())), INVERSE_VIEW_PROJECTION_MATRIX);
+        ctx.renderer->setGlobalMaterial(getBytes(previousViewProjectionMatrix), PREVIOUS_VIEW_PROJECTION_MATRIX);
+        ctx.renderer->setGlobalMaterial(getBytes(glm::vec2(camera->nearPlane, camera->farPlane)), CAMERA_PLANES);
 
+        if (ctx.renderer->useTAAScaling)
+        {
+            ctx.renderer->setGlobalMaterial(getBytes(jitterNDC), JITTER);
+        }
+        else
+        {
+            ctx.renderer->setGlobalMaterial(getBytes(glm::vec2(0.0f)), JITTER);
+        }
+
+        //update previous projection matrix
+        previousViewProjectionMatrix = projectionMatrix * camera->getViewMatrix();
+
+        if (ctx.renderer->useTAAScaling)
+        {
+            projectionMatrix[2][0] += jitterNDC.x;
+            projectionMatrix[2][1] += jitterNDC.y;
+        }
+        ctx.renderer->setGlobalMaterial(getBytes(projectionMatrix * camera->getViewMatrix()), JITTERED_VIEW_PROJECTION_MATRIX);
+        ctx.renderer->setGlobalMaterial(getBytes(glm::inverse(projectionMatrix)), JITTERED_INVERSE_PROJECTION_MATRIX);
 
         ctx.renderer->update(queue, normalizedViewport);
     }
@@ -414,8 +485,7 @@ struct BoundingSphereRequestSystem : ISystem
         {
             auto& comp = world.getComponent<BoundingSphereRequestComponent>(e);
             auto& mesh = world.getComponent<MeshComponent>(e).mesh;
-            auto sphere = mesh->boundingSphere();
-            world.addComponent<BoundingSphereComponent>(e, BoundingSphereComponent(sphere));
+            world.addComponent<BoundingSphereComponent>(e, BoundingSphereComponent(glm::vec4(mesh->boundingSphereCenter(), mesh->boundingSphereRadius())));
             world.removeComponent<BoundingSphereRequestComponent>(e);
         }
     }
@@ -434,9 +504,11 @@ struct UpdateWardMaterialSystem : public ISystem
             // get bytes from material
             auto roughness = getBytes(wardComp.roughness);
             auto metallic = getBytes(wardComp.metallic);
+            auto alpha = getBytes(wardComp.alpha);
             // check if the instance material has this particular material
             ctx.renderer->setInstanceMaterial(mID, roughness, MaterialType::ROUGHNESS);
             ctx.renderer->setInstanceMaterial(mID, metallic, MaterialType::METALLIC);
+            ctx.renderer->setInstanceMaterial(mID, alpha, WARD_ALPHA);
         }
     }
     std::string name() const override { return "Update Ward Material System"; }
@@ -462,14 +534,14 @@ struct UpdateBVH : public ISystem
 {
     void update(World& world, const Context& ctx, float dt) override
     {
-        auto entities = world.query<BVHRequestComponent, MeshComponent>();
+        auto entities = world.query<BVHRequestComponent>();
         for (auto& entity : entities)
         {
-            auto& meshComp = world.getComponent<MeshComponent>(entity);
-            auto mesh = meshComp.mesh;
-
+            auto bvhComp = world.getComponent<BVHRequestComponent>(entity);
+            auto mesh = bvhComp.mesh;
+            if (!mesh) continue; // bvhrequestcomponent has not a valid mesh 
             // build bvh
-            auto bvh = BVH(mesh->getVertices(), mesh->getFacesData(), 8);
+            auto bvh = BVH(mesh->getVertices(), mesh->getFacesData(), bvhComp.primitivesPerLeaf);
 
             // add bvh component
             world.addComponent<BVHComponent>(entity, BVHComponent(bvh, mesh->getVertices(), mesh->getFacesData()));
@@ -488,9 +560,10 @@ struct UpdateBVHMaterial : public ISystem
         auto entities = world.query<BVHComponent, BVHMaterialComponent, MaterialComponent>();
         for (auto& entity : entities)
         {
-            auto bvhMatComp = world.getComponent<BVHMaterialComponent>(entity);
+            auto& bvhMatComp = world.getComponent<BVHMaterialComponent>(entity);
             if (bvhMatComp.dirty)
             {
+                std::cout << "Updating bvh materials" << std::endl;
                 auto bvhComp = world.getComponent<BVHComponent>(entity);
                 auto materialComp = world.getComponent<MaterialComponent>(entity);
                 auto mID = materialComp.id;
@@ -499,11 +572,132 @@ struct UpdateBVHMaterial : public ISystem
                 auto indices = bvhComp.indices;
                 ctx.renderer->setInstanceMaterial(mID, getBytes(bvh.getData()), BVH_NODES);
                 ctx.renderer->setInstanceMaterial(mID, getBytes(bvh.getTriangles()), BVH_PRIMITIVES);
+
+                // set them also as global material
+                ctx.renderer->setGlobalMaterial(getBytes(bvh.getData()), BVH_NODES);
+                ctx.renderer->setGlobalMaterial(getBytes(bvh.getTriangles()), BVH_PRIMITIVES);
+                ctx.renderer->setGlobalMaterial(getBytes(BVHInfo{static_cast<int>(bvh.getData().size()), {}, static_cast<int>(bvh.getTriangles().size())}), BVH_INFO);
+
                 bvhMatComp.dirty = false;
             }
         }
     }
     std::string name() const override { return "Update BVH Material System"; }
+};
+
+struct UpdateRenderingSettings : public ISystem
+{
+    void update(World& world, const Context& ctx, float dt) override
+    {
+        auto entities = world.query<RenderingSettingsComponent>();
+        for (auto& entity : entities)
+        {
+            auto& settings = world.getComponent<RenderingSettingsComponent>(entity);
+            ctx.renderer->setTAAScaling(settings.TAAScaling);
+            ctx.renderer->useTAAScaling = settings.useTAAScaling;
+            ctx.renderer->setGlobalMaterial(getBytes(settings.TAAScaling), TAAScaling);
+        }
+    }
+    std::string name() const override { return "Update BVH Material System"; }
+};
+
+struct UpdateBillboardMaterialsSystem : ISystem
+{
+    void update(World& world, const Context& ctx, float dt) override
+    {
+        auto entities = world.query<BillboardMaterialComponent, MaterialComponent>();
+        for (auto& entity : entities)
+        {
+            auto& billboardComp = world.getComponent<BillboardMaterialComponent>(entity);
+            auto mID = world.getComponent<MaterialComponent>(entity).id;
+            // get bytes from material
+            auto size = getBytes(billboardComp.material);
+            // check if the instance material has this particular material
+            ctx.renderer->setInstanceMaterial(mID, size, MaterialType::BILLBOARD_DATA);
+        }
+    }
+    std::string name() const override { return "Update Billboard Material System"; }
+};
+
+struct MoveBillboards : ISystem
+{
+    void update(World& world, const Context& ctx, float dt) override
+    {
+        auto entities = world.query<BillboardMaterialComponent, Transform>();
+        for (auto& entity : entities)
+        {
+            auto& billboardComp = world.getComponent<BillboardMaterialComponent>(entity);
+            auto transform = world.getComponent<Transform>(entity);
+            billboardComp.material.position = glm::vec4(transform.position, 1.0f);
+        }
+    }
+    std::string name() const override { return "Move Bilboards System"; }
+};
+
+struct MoveChildren : ISystem
+{
+    void update(World& world, const Context& ctx, float dt) override
+    {
+        auto entities = world.query<Transform, ChildComponent>();
+        for (auto& entity: entities)
+        {
+            auto childComponent = world.getComponent<ChildComponent>(entity);
+            auto parentID = childComponent.parent;
+            auto localTransform = childComponent.localTransform;
+            auto& childGlobalTransform = world.getComponent<Transform>(entity);
+            if (world.hasEntity(parentID) and world.hasComponent<Transform>(childComponent.parent))
+            {
+                // parent exists and has a component
+                auto parentTransform = world.getComponent<Transform>(childComponent.parent);
+                childGlobalTransform.position = parentTransform.position + (parentTransform.rotation * localTransform.position);
+                childGlobalTransform.rotation = parentTransform.rotation * localTransform.rotation;
+            }
+        }
+    }
+    std::string name() const override { return "Move Children System"; }
+};
+
+struct UpdateUnlitMaterial : ISystem
+{
+    void update(World& world, const Context& ctx, float dt) override
+    {
+        auto entities = world.query<UnlitMaterialComponent, MaterialComponent>();
+        for (auto& entity : entities)
+        {
+            auto mID = world.getComponent<MaterialComponent>(entity).id;
+            auto unlitMaterial = world.getComponent<UnlitMaterialComponent>(entity);
+            ctx.renderer->setInstanceMaterial(mID, getBytes(unlitMaterial.color), MaterialType::UNLIT_COLOR);
+        }
+    }
+    std::string name() const override { return "Update Unlit Material System"; }
+};
+
+struct UpdateParticleMaterial : ISystem
+{
+    void update(World& world, const Context& ctx, float dt) override
+    {
+        auto entities = world.query<ParticleComponent, MaterialComponent>();
+        for (auto& entity : entities)
+        {
+            auto mID = world.getComponent<MaterialComponent>(entity).id;
+            auto material = world.getComponent<ParticleComponent>(entity);
+            ctx.renderer->setInstanceMaterial(mID, getBytes(material.softness), MaterialType::PARTICLE_SOFTNESS);
+        }
+    }
+    std::string name() const override { return "Update Particle Material System"; }
+};
+
+struct UpdateAtmosphereSettings : ISystem
+{
+    void update(World& world, const Context& ctx, float dt) override
+    {
+        auto entities = world.query<AtmosphereSettingsComponent>();
+        for (auto& entity : entities)
+        {
+            ctx.renderer->setGlobalMaterial(getBytes(world.getComponent<AtmosphereSettingsComponent>(entity).settings), ATMOSPHERE_SETTINGS);
+        }
+    }
+    std::string name() const override { return "Update Atmosphere Settings System"; }
 };
 
 
